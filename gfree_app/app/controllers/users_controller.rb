73c50cd644baa1dc49b110f16d4ee3232ad57d7b
Beta-23 <alfred.dominguez@gmail.com>
2016-02-22 16:07:29 -0800

    adds schema and index/new erb files

---
+++ gfree_app/app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+def index
+  @users = User.all #this method grabs the all the users
+  render :index
+  end
+
+def new
+  @user = User.new
+  render :new
+
+  end  
+
+def create
+  user_params = params.require(:user).permit(:first_name, :last_name, :email, :type_allergy, :password)
+  @user = User.create(user_params)
+
+    redirect_to "/users"
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render :show
+    
+  end
+
+end
